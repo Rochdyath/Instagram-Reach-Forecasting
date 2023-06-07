22a06bad0ddd96e41f182687023c628aea5682a6
Rochdyath <rochdiath.bachabi@epitech.eu>
2023-06-07 15:27:40 +0100

    feat: Instagram Reach Trend

--- insta_reach.py
+++ insta_reach.py
@@ -5,4 +5,10 @@ pio.templates.default = "plotly_white"
 
 data = pd.read_csv("Instagram-Reach.csv", encoding = 'latin-1')
 data['Date'] = pd.to_datetime(data['Date'])
-print(data.head())
+data = data.rename(columns={"Instagram reach": "Reach"})
+
+def insta_trend():
+    fig = px.line(data, x='Date', y='Reach', title='Instagram Reach Trend')
+    fig.show()
+
+insta_trend()
